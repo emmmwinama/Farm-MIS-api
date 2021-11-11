c9893a974a24318ade663887eadf6040e725df95
Emmanuel Mwinama <bit15-emwinama@poly.ac.mw>
2021-11-11 09:57:02 -0800

    Adding paging to findByName method

--- src/main/java/tech/emmmwinama/groproapi/repositories/CropRepository.java
+++ src/main/java/tech/emmmwinama/groproapi/repositories/CropRepository.java
@@ -1,11 +1,11 @@
 package tech.emmmwinama.groproapi.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import tech.emmmwinama.groproapi.models.Crop;
 
-import java.util.List;
-
 public interface CropRepository extends CrudRepository<Crop, Long> {
-    List<Crop> findByName(@Param("name") String name);
+    Page<Crop> findByName(@Param("name") String name, Pageable pageable);
 }
