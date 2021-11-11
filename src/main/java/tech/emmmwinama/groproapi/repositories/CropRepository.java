f19dc62daf4519449edfb52d04568b4fd084aa1c
Emmanuel Mwinama <bit15-emwinama@poly.ac.mw>
2021-11-11 11:08:47 +0200

    Fixing things

--- src/main/java/tech/emmmwinama/groproapi/repositories/CropRepository.java
+++ src/main/java/tech/emmmwinama/groproapi/repositories/CropRepository.java
@@ -7,13 +7,5 @@ import org.springframework.data.repository.query.Param;
 import tech.emmmwinama.groproapi.models.Crop;
 
 public interface CropRepository extends CrudRepository<Crop, Long> {
-<<<<<<< HEAD
     Page<Crop> findByName(@Param("name") String name, Pageable pageable);
-=======
-<<<<<<< HEAD
-    List<Crop> findByName(String name);
-=======
-    List<Crop> findByName(@Param("name") String name);
->>>>>>> 157e320cca9a4d3514ccadc9dff3eb50a43f636c
->>>>>>> b69b3e895dbeb3c667bb410d4261660e75889ae4
 }
