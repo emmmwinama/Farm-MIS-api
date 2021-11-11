157e320cca9a4d3514ccadc9dff3eb50a43f636c
Emmanuel Mwinama <bit15-emwinama@poly.ac.mw>
2021-11-11 08:45:17 -0800

    added a findByName function to Crop repositories

--- src/main/java/tech/emmmwinama/groproapi/repositories/CropRepository.java
+++ src/main/java/tech/emmmwinama/groproapi/repositories/CropRepository.java
@@ -1,7 +1,11 @@
 package tech.emmmwinama.groproapi.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import tech.emmmwinama.groproapi.models.Crop;
 
+import java.util.List;
+
 public interface CropRepository extends CrudRepository<Crop, Long> {
+    List<Crop> findByName(@Param("name") String name);
 }
